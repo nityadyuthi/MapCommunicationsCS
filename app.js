df21e494e2a15c856489734eadf6be1ba27242b1
nityadyuthi <nityaa55@gmail.com>
2021-05-07 16:13:25 +0530

    finish up supervisor side.

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+
+var multer = require("multer");
+var upload = multer({
+  dest: "uploads/",
+});
+var cpUpload = upload.fields([{ name: "avatar", maxCount: 8 }]);
+
 const ejs = require("ejs");
 let mysql = require("mysql");
-
+var isLoggedIn = false;
 const app = express();
 
 app.use(express.static("public"));
@@ -14,6 +21,17 @@ app.use(
     extended: true,
   })
 );
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+var upload = multer({ storage: storage });
 
 let connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -29,8 +47,32 @@ connection.connect(function (err) {
   console.log("Connected to the MySQL server.");
 });
 
+app.get("/", (req, res) => {
+  res.redirect("user");
+});
+
 app.get("/user", (req, res) => {
   res.render("user", { data: 0, message: "" });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/supervisor", (req, res) => {
+  if (isLoggedIn) {
+    res.render("supervisor", { message: "" });
+  }
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  var username = req.body.username;
+  var password = req.body.password;
+  if ((username = "admin" && password == "root")) {
+    isLoggedIn = true;
+    res.redirect("supervisor");
+  }
 });
 
 app.post("/user", (req, res) => {
@@ -50,13 +92,23 @@ app.post("/user", (req, res) => {
         console.log(result.insertId);
         res.render("user", {
           message:
-            "Your query has been submitted. The request ID is:" +
+            "Your query has been submitted. Our representatives will get back to you shortly. The request ID is:" +
             result.insertId,
           data: 1,
         });
       }
     }
   );
+});
+
+app.post("/supervisor", upload.array("file", 10), (req, res) => {
+  console.log(req.files);
+  let file = "";
+  req.files.forEach((ele) => {
+    var name = ele.filename.toString();
+    file = file.concat(name);
+  });
+  res.render("supervisor", { message: "Uploaded successfully" });
 });
 
 app.listen(3000, function () {
